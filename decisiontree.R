1874dfc16fce8177344b4f74409c45379d20ae78
swtzang <swtzang@gmail.com>
2019-07-20 23:20:49 +0800

    Add files via upload

---
+++ decisiontree.R
@@ -0,0 +1,14 @@
+#Read data file
+mydata <- read.csv("~/Dropbox/Public/Cardiotocographic.csv")
+mydata$NSPF <- as.factor(mydata$NSP)
+
+#Decision tree with party
+library(party)
+mytree <- ctree(NSPF~LB+AC+FM, mydata, controls=ctree_control(mincriterion=0.9, minsplit=50))
+print(mytree)
+plot(mytree,type="simple")
+
+#Misclassification error
+tab<-table(predict(mytree), mydata$NSPF)
+print(tab)
+1-sum(diag(tab))/sum(tab)
