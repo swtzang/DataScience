b34fcaabc8a1a5c5139ce86ba5388d65365733b3
swtzang <swtzang@example.com>
2019-12-09 09:44:16 +0800

    1209

---
+++ ML_stock selection/DMwR2.R
@@ -0,0 +1,317 @@
+# Data mining with R Leaning with cases
+# Chapter 5 : Predicting stock returns
+# https://ltorgo.github.io/DMwR2/Rstocks.html#what_to_predict
+
+rm(list=ls())
+
+library(DMwR2)
+library(xts)
+library(ggplot2)
+library(grid)
+library(quantmod)
+
+data(GSPC, package="DMwR2")
+first(GSPC)
+last(GSPC)
+
+library(quantmod)
+library(tbl2xts)
+# GSPC <- getSymbols("^GSPC",auto.assign=FALSE)
+# AAPL <- read.csv("../ML_stock selection/AAPL_2007_2019.csv")
+# AAPL <- as.xts(AAPL[, c(-1, -2)], order.by = as.Date(AAPL[,1], format = '%Y-%m-%d'))
+# GSPC <- AAPL
+
+
+# GSPC <- getSymbols("^GSPC",from="1970-01-02",to="2016-01-25",auto.assign=FALSE)
+class(GSPC)
+#### sub-section:  What to Predict?
+
+##
+# quotes <- GSPC
+T.ind <- function(quotes, tgt.margin = 0.025, n.days = 10) {
+  v <- apply(HLC(quotes), 1, mean)
+  v[1] <- Cl(quotes)[1]
+  
+  r <- matrix(NA, ncol = n.days, nrow = NROW(quotes))
+  # x = 1
+  for (i in 1:n.days) r[, i] <- Next(Delt(v, k = i), i)
+  
+  y <- apply(r, 1, function(x) sum(x[x > tgt.margin | x < -tgt.margin]))
+  
+  if (is.xts(quotes)) xts(y, time(quotes)) else y
+}
+
+candleChart(last(GSPC,'3 months'),theme='white', TA=NULL)
+avgPrice <- function(p) apply(HLC(p),1,mean)
+addAvgPrice <- newTA(FUN=avgPrice,col=1,legend='AvgPrice')
+addT.ind <- newTA(FUN=T.ind,col='red', legend='tgtRet')
+addAvgPrice(on=1) 
+addT.ind()
+
+##
+avgPrice <- function(p) apply(HLC(p), 1, mean) 
+addAvgPrice <- newTA(FUN=avgPrice, col=1, legend='AvgPrice')
+addT.ind <- newTA(FUN=T.ind, col='red', legend='tgtRet')
+candleChart(last(GSPC,'3 months'), theme='white', TA=c(addAvgPrice(on=1), addT.ind()))
+#
+
+
+#### sub-section:  Which Predictors?
+
+##
+library(TTR)
+myATR        <- function(x) ATR(HLC(x))[,'atr']
+mySMI        <- function(x) SMI(HLC(x))[, "SMI"]
+myADX        <- function(x) ADX(HLC(x))[,'ADX']
+myAroon      <- function(x) aroon(cbind(Hi(x),Lo(x)))$oscillator
+myBB         <- function(x) BBands(HLC(x))[, "pctB"]
+myChaikinVol <- function(x) Delt(chaikinVolatility(cbind(Hi(x),Lo(x))))[, 1]
+myCLV        <- function(x) EMA(CLV(HLC(x)))[, 1]
+myEMV        <- function(x) EMV(cbind(Hi(x),Lo(x)),Vo(x))[,2]
+myMACD       <- function(x) MACD(Cl(x))[,2]
+myMFI        <- function(x) MFI(HLC(x),  Vo(x))
+mySAR        <- function(x) SAR(cbind(Hi(x),Cl(x))) [,1]
+myVolat      <- function(x) volatility(OHLC(x),calc="garman")[,1]
+
+##
+library(randomForest)
+data.model <- specifyModel(T.ind(GSPC) ~ Delt(Cl(GSPC),k=1:10) + 
+                             myATR(GSPC) + mySMI(GSPC) + myADX(GSPC) + myAroon(GSPC) + 
+                             myBB(GSPC)  + myChaikinVol(GSPC) + myCLV(GSPC) + 
+                             CMO(Cl(GSPC)) + EMA(Delt(Cl(GSPC))) + myEMV(GSPC) + 
+                             myVolat(GSPC)  + myMACD(GSPC) + myMFI(GSPC) + RSI(Cl(GSPC)) +
+                             mySAR(GSPC) + runMean(Cl(GSPC)) + runSD(Cl(GSPC)))
+
+#T.ind(GSPC)
+
+set.seed(1234)
+rf <- buildModel(data.model,method='randomForest',
+                 training.per=c("1995-01-01","2005-12-30"),
+                 ntree=1000, importance=TRUE)
+
+##
+ex.model <- specifyModel(T.ind(IBM) ~ Delt(Cl(IBM), k = 1:3))
+data <- modelData(ex.model, data.window = c("2009-01-01",  "2009-08-10"))
+m <- myFavouriteModellingTool(ex.model@model.formula, as.data.frame(data))
+#
+varImpPlot(rf@fitted.model, type = 1)
+# 1 = mean decrease in accuracy
+imp <- importance(rf@fitted.model, type = 1)
+rownames(imp)[which(imp > 30)]
+#[1] "myATR.GSPC"      "mySMI.GSPC"      "myADX.GSPC"      "myAroon.GSPC"    "myEMV.GSPC"      "myVolat.GSPC"    "myMACD.GSPC"     "myMFI.GSPC"     
+#[9] "mySAR.GSPC"      "runMean.Cl.GSPC" "runSD.Cl.GSPC"  
+#
+data.model <- specifyModel(T.ind(GSPC) ~ myATR(GSPC) + mySMI(GSPC) +  myADX(GSPC) + 
+                             myAroon(GSPC) + myEMV(GSPC) + myVolat(GSPC) + 
+                             myMACD(GSPC) + myMFI(GSPC) + mySAR(GSPC) + 
+                             runMean(Cl(GSPC)) + runSD(Cl(GSPC)))
+
+## The regression task
+Tdata.train <- as.data.frame(modelData(data.model, data.window=c('1970-01-02','2005-12-30')))
+Tdata.eval <- na.omit(as.data.frame(modelData(data.model, data.window=c('2006-01-01','2016-01-25'))))
+Tform <- as.formula('T.ind.GSPC ~ .')
+## The classification task
+buy.thr <- 0.1
+sell.thr <- -0.1
+Tdata.trainC <- cbind(Signal=trading.signals(Tdata.train[["T.ind.GSPC"]], buy.thr, sell.thr), Tdata.train[,-1])
+Tdata.evalC <-  cbind(Signal=trading.signals(Tdata.eval[["T.ind.GSPC"]],  buy.thr,sell.thr), Tdata.eval[,-1])
+
+TformC <- as.formula("Signal ~ .")
+
+# The Modeling Tools
+set.seed(1234)
+library(nnet)
+## The first column is the target variable
+norm.data <- data.frame(T.ind.GSPC=Tdata.train[[1]],scale(Tdata.train[,-1]))
+nn <- nnet(Tform, norm.data[1:1000, ], size = 5, decay = 0.01, 
+           maxit = 1000, linout = TRUE, trace = FALSE)
+preds <- predict(nn, norm.data[1001:2000, ])
+#
+sigs.nn <- trading.signals(preds, 0.1, -0.1)
+true.sigs <- trading.signals(Tdata.train[1001:2000, "T.ind.GSPC"], 0.1, -0.1)
+sigs.PR(sigs.nn,true.sigs)
+#
+set.seed(1234)
+library(nnet)
+norm.data <- data.frame(Signal=Tdata.trainC$Signal,scale(Tdata.trainC[,-1]))
+nn <- nnet(Signal ~ ., norm.data[1:1000, ], size = 10, decay = 0.01, 
+           maxit = 1000, trace = FALSE)
+preds <- predict(nn, norm.data[1001:2000, ], type = "class")
+
+sigs.PR(preds, norm.data[1001:2000, 1])
+
+#
+set.seed(1234)
+library(e1071)
+sv <- svm(Tform, Tdata.train[1:1000, ], gamma = 0.001, cost = 100)
+s.preds <- predict(sv, Tdata.train[1001:2000, ])
+sigs.svm <- trading.signals(s.preds, 0.1, -0.1)
+true.sigs <- trading.signals(Tdata.train[1001:2000, "T.ind.GSPC"], 0.1, -0.1)
+sigs.PR(sigs.svm, true.sigs)
+#
+library(kernlab)
+ksv <- ksvm(Signal ~ ., Tdata.trainC[1:1000, ], C = 10)
+ks.preds <- predict(ksv, Tdata.trainC[1001:2000, ])
+sigs.PR(ks.preds, Tdata.trainC[1001:2000, 1])
+#
+library(earth)
+e <- earth(Tform, Tdata.train[1:1000, ])
+e.preds <- predict(e, Tdata.train[1001:2000, ])
+sigs.e <- trading.signals(e.preds, 0.1, -0.1)
+true.sigs <- trading.signals(Tdata.train[1001:2000, "T.ind.GSPC"],  0.1, -0.1)
+sigs.PR(sigs.e, true.sigs)
+
+summary(e)
+
+evimp(e, trim=FALSE)
+
+#
+policy.1 <- function(signals,market,opened.pos,money,
+                     bet=0.2,hold.time=10,
+                     exp.prof=0.025, max.loss= 0.05
+)
+{
+  d <- NROW(market) # this is the ID of today
+  orders <- NULL
+  nOs <- NROW(opened.pos)
+  # nothing to do!
+  if (!nOs && signals[d] == 'h') return(orders)
+  
+  # First lets check if we can open new positions
+  # i) long positions
+  if (signals[d] == 'b' && !nOs) {
+    quant <- round(bet*money/Cl(market)[d],0)
+    if (quant > 0) 
+      orders <- rbind(orders,
+                      data.frame(order=c(1,-1,-1),order.type=c(1,2,3), 
+                                 val = c(quant,
+                                         Cl(market)[d]*(1+exp.prof),
+                                         Cl(market)[d]*(1-max.loss)
+                                 ),
+                                 action = c('open','close','close'),
+                                 posID = c(NA,NA,NA)
+                      )
+      )
+    
+    # ii) short positions  
+  } else if (signals[d] == 's' && !nOs) {
+    # this is the nr of stocks we already need to buy 
+    # because of currently opened short positions
+    need2buy <- sum(opened.pos[opened.pos[,'pos.type']==-1,
+                               "N.stocks"])*Cl(market)[d]
+    quant <- round(bet*(money-need2buy)/Cl(market)[d],0)
+    if (quant > 0)
+      orders <- rbind(orders,
+                      data.frame(order=c(-1,1,1),order.type=c(1,2,3), 
+                                 val = c(quant,
+                                         Cl(market)[d]*(1-exp.prof),
+                                         Cl(market)[d]*(1+max.loss)
+                                 ),
+                                 action = c('open','close','close'),
+                                 posID = c(NA,NA,NA)
+                      )
+      )
+  }
+  
+  # Now lets check if we need to close positions
+  # because their holding time is over
+  if (nOs) 
+    for(i in 1:nOs) {
+      if (d - opened.pos[i,'Odate'] >= hold.time)
+        orders <- rbind(orders,
+                        data.frame(order=-opened.pos[i,'pos.type'],
+                                   order.type=1,
+                                   val = NA,
+                                   action = 'close',
+                                   posID = rownames(opened.pos)[i]
+                        )
+        )
+    }
+  
+  orders
+}
+
+#
+policy.2 <- function(signals,market,opened.pos,money,
+                     bet=0.2,exp.prof=0.025, max.loss= 0.05
+)
+{
+  d <- NROW(market) # this is the ID of today
+  orders <- NULL
+  nOs <- NROW(opened.pos)
+  # nothing to do!
+  if (!nOs && signals[d] == 'h') return(orders)
+  
+  # First lets check if we can open new positions
+  # i) long positions
+  if (signals[d] == 'b') {
+    quant <- round(bet*money/Cl(market)[d],0)
+    if (quant > 0) 
+      orders <- rbind(orders,
+                      data.frame(order=c(1,-1,-1),order.type=c(1,2,3), 
+                                 val = c(quant,
+                                         Cl(market)[d]*(1+exp.prof),
+                                         Cl(market)[d]*(1-max.loss)
+                                 ),
+                                 action = c('open','close','close'),
+                                 posID = c(NA,NA,NA)
+                      )
+      )
+    
+    # ii) short positions  
+  } else if (signals[d] == 's') {
+    # this is the money already committed to buy stocks
+    # because of currently opened short positions
+    need2buy <- sum(opened.pos[opened.pos[,'pos.type']==-1,
+                               "N.stocks"])*Cl(market)[d]
+    quant <- round(bet*(money-need2buy)/Cl(market)[d],0)
+    if (quant > 0)
+      orders <- rbind(orders,
+                      data.frame(order=c(-1,1,1),order.type=c(1,2,3), 
+                                 val = c(quant,
+                                         Cl(market)[d]*(1-exp.prof),
+                                         Cl(market)[d]*(1+max.loss)
+                                 ),
+                                 action = c('open','close','close'),
+                                 posID = c(NA,NA,NA)
+                      )
+      )
+  }
+  
+  orders
+}
+
+## Train and test periods
+start <- 1
+len.tr <- 1000
+len.ts <- 500
+tr <- start:(start+len.tr-1)
+ts <- (start+len.tr):(start+len.tr+len.ts-1)
+## getting the quotes for the testing period
+data(GSPC)
+date <- rownames(Tdata.train[start+len.tr,])
+marketTP <- GSPC[paste(date,'/',sep='')][1:len.ts]
+## learning the model and obtaining its signal predictions for the test period
+library(e1071)
+s <- svm(Tform, Tdata.train[tr,], cost=10,gamma=0.01)
+p <- predict(s, Tdata.train[ts,])
+sig <- trading.signals(p, 0.1, -0.1)
+## now using the simulated trader during the testing period
+t1 <- trading.simulator(marketTP, signals=sig, policy.func='policy.1',
+                        policy.pars=list(exp.prof=0.05,bet=0.2,hold.time=30))
+t1
+summary(t1)
+#
+tradingEvaluation(t1)
+
+plot(t1, marketTP,  theme = "white",  name = "SP500")
+#
+t2 <- trading.simulator(marketTP, sig, "policy.2", list(exp.prof = 0.05, bet = 0.3))
+summary(t2)
+tradingEvaluation(t2)
+
+plot(t2, marketTP,  theme = "white",  name = "SP500")
+
+
+
+
---
+++ webscaping/Russell2000_component_stock_scraping.R
@@ -0,0 +1,295 @@
+# Russell2000 constituent stock names list web scraping
+# https://github.com/mdancho84/analysis_Russell2000/blob/master/russell2000_analysis.R
+# Prerequisites ----------------------------------------------------------------
+library(rvest)
+library(quantmod)
+library(tidyverse)
+library(stringr)
+library(lubridate)
+library(modelr)
+library(plotly)
+
+######################################## Part 1 ################################
+
+# Web Scraping: Get the List of Russell 2000 Stocks ----------------------------
+
+# Base path and page rows from www.marketvolume.com
+base_path <- "http://www.marketvolume.com/indexes_exchanges/r2000_components.asp?s=RUT&row="
+row_num <- seq(from = 0, by = 250, length.out = 9)
+
+stocklist <- get_stocklist(base_path, row_num)
+
+# row_num = 0
+# Function to web scrape stocks from individual pages
+get_stocklist <- function(base_path, row_num) {
+  path <- paste0(base_path, row_num)
+  # Get raw table
+  stock_table <- read_html(path) %>%
+    html_node("table") %>%
+    html_table() 
+  # Format table
+  stock_table <- stock_table[-1, 1:2] %>%
+    as_tibble() %>%
+    rename(symbol = X1, company = X2)
+  stock_table
+}
+
+# Map get_stocklist() function, unnest and tidy
+stocklist <- tibble(row_num) %>%
+  mutate(
+    stock_table = map(row_num, 
+                      function(.x) get_stocklist(base_path = base_path,
+                                                 row_num = .x)
+    )
+  ) %>%
+  unnest() %>%
+  select(-row_num) %>%
+  mutate_all(function(x) str_trim(x, side = 'both') %>% str_to_upper()) %>%
+  distinct()
+
+
+# Function Mapping with quantmod and purrr -------------------------------------
+
+# Create functions to get stock prices and log returns
+get_stock_prices <- function(ticker, return_format = "tibble", ...) {
+  # Get stock prices
+  stock_prices_xts <- getSymbols(Symbols = ticker, auto.assign = FALSE, ...)
+  # Rename
+  names(stock_prices_xts) <- c("Open", "High", "Low", "Close", "Volume", "Adjusted")
+  # Return in xts format if tibble is not specified
+  if (return_format == "tibble") {
+    stock_prices <- stock_prices_xts %>%
+      as_tibble() %>%
+      rownames_to_column(var = "Date") %>%
+      mutate(Date = ymd(Date))
+  } else {
+    stock_prices <- stock_prices_xts
+  }
+  
+  stock_prices
+}
+
+get_log_returns <- function(x, return_format = "tibble", period = 'daily', ...) {
+  # Convert tibble to xts
+  if (!is.xts(x)) {
+    x <- xts(x[,-1], order.by = x$Date)
+  }
+  # Get stock prices
+  log_returns_xts <- periodReturn(x = x$Adjusted, type = 'log', period = period, ...)
+  # Rename
+  names(log_returns_xts) <- "Log.Returns"
+  # Return in xts format if tibble is not specified
+  if (return_format == "tibble") {
+    log_returns <- log_returns_xts %>%
+      as_tibble() %>%
+      rownames_to_column(var = "Date") %>%
+      mutate(Date = ymd(Date))
+  } else {
+    log_returns <- log_returns_xts
+  }
+  log_returns
+}
+
+# Use purrr to map the functions across the entire list of Russell 2000 stocks
+# Warning: This takes about 15 minutes 
+# Start the clock!
+ptm <- proc.time()
+from <- "2007-01-01"
+to   <- today()
+stocklist <- stocklist %>%
+  mutate(
+    stock.prices = map(symbol,
+                       function(.x) tryCatch({
+                         get_stock_prices(.x,
+                                          return_format = "tibble",
+                                          from = from,
+                                          to   = to)
+                       }, error = function(e) {
+                         NA
+                       })
+    ),
+    len = map_int(stock.prices, length)
+  ) %>%
+  filter(len > 1) %>%
+  select(-len) %>%
+  mutate(
+    log.returns  = map(stock.prices,
+                       function(.x) get_log_returns(.x, return_format = "tibble")),
+    mean.log.returns = map_dbl(log.returns, ~ mean(.$Log.Returns)),
+    sd.log.returns   = map_dbl(log.returns, ~ sd(.$Log.Returns)),
+    n.trade.days = map_dbl(stock.prices, nrow)
+  )
+# Stop the clock
+proc.time() - ptm
+
+
+# Visualize the Relationship between Std Dev and Mean --------------------------
+trade_day_thresh <- max(stocklist$n.trade.days)
+sd_limit <- 0.075
+stocklist %>%
+  filter(n.trade.days >= trade_day_thresh,
+         sd.log.returns <= sd_limit) %>%
+  ggplot(aes(x = sd.log.returns, y = mean.log.returns)) +
+  geom_point(alpha = 0.1) +
+  geom_smooth() +
+  labs(title = "Market Tends to Penalize Stocks with Large SDDLR",
+       subtitle = "Best to Focus on Stocks with Highest Ratio of MDLR to SDDLR",
+       x = "Standard Deviation of Daily Log Returns (SDDLR)",
+       y = "Mean Daily Log Returns (MDLR)")
+
+
+# Develop Reward-to-Risk Metric ------------------------------------------------
+stocklist <- stocklist %>%
+  mutate(reward.metric = (mean.log.returns * n.trade.days) / sd.log.returns)
+
+
+# Visualize the Results with Plotly --------------------------------------------
+
+# Inputs
+trade_day_thresh <- max(stocklist$n.trade.days)
+lab <- "Russell 2000"
+back_col <- '#2C3E50'
+font_col <- '#FFFFFF'
+line_col <- "#FFFFFF"
+grid_col <- 'rgb(255, 255, 255)'
+col_brew_pal <- 'BrBG'
+# Plotly
+plot_ly(data   = stocklist %>% filter(n.trade.days >= trade_day_thresh),
+        type   = "scatter",
+        mode   = "markers",
+        x      = ~ sd.log.returns,
+        y      = ~ mean.log.returns,
+        color  = ~ reward.metric,
+        colors = col_brew_pal,
+        size   = ~ reward.metric,
+        text   = ~ str_c("<em>", company, "</em><br>",
+                         "Ticker: ", symbol, "<br>",
+                         "No. of Trading Days: ", n.trade.days, "<br>",
+                         "Reward to Risk: ", round(reward.metric, 1)),
+        marker = list(opacity = 0.8,
+                      symbol = 'circle',
+                      sizemode = 'diameter',
+                      sizeref = 4.0,
+                      line = list(width = 2, color = line_col))
+) %>%
+  layout(title   = str_c(lab, 'Analysis: Stock Risk vs Reward', sep = " "),
+         xaxis   = list(title = 'Risk: StDev of Daily Log Returns (SDDLR)',
+                        gridcolor = grid_col,
+                        zerolinewidth = 1,
+                        ticklen = 5,
+                        gridwidth = 2),
+         yaxis   = list(title = 'Reward: Mean Daily Log Returns (MDLR)',
+                        gridcolor = grid_col,
+                        zerolinewidth = 1,
+                        ticklen = 5,
+                        gridwith = 2),
+         margin = list(l = 100,
+                       t = 100,
+                       b = 100),
+         font   = list(color = font_col),
+         paper_bgcolor = back_col,
+         plot_bgcolor = back_col)
+
+
+######################################## Part 2 ################################
+
+# Trim List to Top 15 High Performers ------------------------------------------
+
+# Filter high performing stocks
+top_n_limit <- 15
+hp <- stocklist %>%
+  mutate(rank = reward.metric %>% desc() %>% min_rank()) %>%
+  filter(rank <= top_n_limit) %>%
+  arrange(rank)
+
+# Function to return MDLR by year
+means_by_year <- function(log.returns) {
+  log.returns %>%
+    mutate(year = year(Date)) %>%
+    group_by(year) %>%
+    summarize(mean.log.returns = mean(Log.Returns))
+}
+# Map function to data frame
+hp <- hp %>%
+  mutate(means.by.year = map(log.returns, means_by_year))
+
+# Unnest high performing stocks
+hp_unnest <- hp %>%
+  select(symbol, means.by.year) %>%
+  unnest()
+
+# Visualize using ggplot
+hp_unnest %>%
+  ggplot(aes(x = year, y = mean.log.returns)) +
+  geom_ref_line(h = 0) +
+  geom_line(aes(col = symbol)) +
+  geom_smooth(method = "lm", se = FALSE) +
+  facet_wrap(~ symbol, nrow = 3) +
+  theme(legend.position = "None", axis.text.x = element_text(angle=90)) +
+  labs(title = "Best Prospects Have Consistent, Above-Zero MDLR and Growth",
+       subtitle = "Trend Flat to Upward Indicates Growth",
+       x = "Year",
+       y = "Mean Daily Log Returns (MDLR)")
+
+
+# Compute Three Attributes of High Performing Stocks ---------------------------
+
+# Attribute 1: Number of Times MDLR by Year Drops Below Zero
+# Function to return number of times a stock's MDLR by year drops below zero
+means_below_zero <- function(means.by.year) {
+  means.by.year %>%
+    filter(mean.log.returns < 0) %>%
+    nrow()
+}
+# Map function to data frame for all stocks
+hp <- hp %>%
+  mutate(means.below.zero = map_dbl(means.by.year, means_below_zero))
+
+
+# Attribute 2: Slope of MDLR by Year
+# Function to return linear model
+means_by_year_model <- function(means.by.year) {
+  lm(mean.log.returns ~ year, data = means.by.year)
+}
+# Function to return slope of linear model
+slope <- function(means.by.year.model) {
+  means.by.year.model$coefficients[[2]]
+}
+# Map modeling and slope functions
+hp <- hp %>%
+  mutate(
+    means.by.year.model = map(means.by.year, means_by_year_model),
+    slope = map_dbl(means.by.year.model, slope)
+  )
+
+# Attribute 3: Standard deviation of MDLR by year
+sd_of_means_by_year <- function(means.by.year) {
+  sd(means.by.year$mean.log.returns)
+}
+# Map to data frame
+hp <- hp %>%
+  mutate(sd.of.means.by.year = map_dbl(means.by.year, sd_of_means_by_year))
+
+
+# Develop Growth-to-Consistency Metric -----------------------------------------
+
+hp <- hp %>%
+  mutate(growth.metric = slope /((means.below.zero + 1) * sd.of.means.by.year)) 
+
+
+# Visualize Performance of Top Six Stocks --------------------------------------
+
+top_n_limit <- 6
+hp %>%
+  mutate(rank = growth.metric %>% desc() %>% min_rank()) %>%
+  filter(rank <= top_n_limit) %>%
+  select(symbol, stock.prices) %>%
+  unnest() %>%
+  ggplot(aes(x = Date, y = Adjusted, col = symbol)) +
+  geom_line() +
+  facet_wrap(~ symbol, nrow = 3, scales = "free_y") +
+  theme(legend.position = "None") +
+  labs(title = "Prospecting Best Russell 2000 Stocks",
+       subtitle = "Six Small Caps with Amazing Growth, Most Consistency",
+       x = "Year",
+       y = "Price per Share")
